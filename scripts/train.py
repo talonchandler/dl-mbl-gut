89a31dd51b020832cd826a23e2897f24db1ff414
Gloria <glau2@illinois.edu>
2024-08-30 01:25:42 +0000

    Added Dice Coefficient function, set loss = Dice, optimizer = AdamW

--- scripts/train.py
+++ scripts/train.py
@@ -1,12 +1,24 @@
 import torch
 import numpy as np
+import torch.nn as nn
+from PIL import Image
+
+class DiceCoefficient(nn.Module):
+    def __init__(self, eps=1e-6):
+        super().__init__()
+        self.eps = eps
+
+    # the dice coefficient of two sets represented as vectors a, b can be
+    # computed as (2 *|a b| / (a^2 + b^2))
+    def forward(self, prediction, target):
+        intersection = (prediction*target).sum()
+        union = (prediction*prediction).sum() + (target*target).sum()
+        return 2 * intersection / union.clamp(min=self.eps)
 
 
 def train(
     model,
     loader,
-    optimizer,
-    loss_function,
     epoch,
     log_interval=100,
     log_image_interval=20,
@@ -14,8 +26,9 @@ def train(
     device=None,
     early_stop=False,
 ):
-    optimizer = torch.optim.AdamW(model.parameters())
-    loss_function = torch.nn.BCEWithLogitsLoss()
+    optimizer = torch.optim.AdamW(model.parameters(), lr = .004)
+    loss_function = DiceCoefficient()
+
     if device is None:
         # You can pass in a device or we will default to using
         # the gpu. Feel free to try training on the cpu to see
