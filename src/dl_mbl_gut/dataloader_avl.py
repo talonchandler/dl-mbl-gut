d70bbd01883ab492d24a6b26dbf8bc223e6c8a89
Aaron van Loon <aaronpvanloon@g.ucla.edu>
2024-08-31 21:17:01 +0000

    kinda finished dataloader_avl

--- src/dl_mbl_gut/dataloader_avl.py
+++ src/dl_mbl_gut/dataloader_avl.py
@@ -5,7 +5,7 @@ import torch
 from torch.utils.data import Dataset
 import pandas as pd
 from monai import transforms
-
+from monai.data import MetaTensor
 class NucleiDataset(Dataset):
     """A PyTorch dataset to load cell images and nuclei masks"""
 
@@ -41,20 +41,22 @@ class NucleiDataset(Dataset):
         image = TiffReader(img_path).data[1,:,:,:]
         print('opened ' + img_path)
         image = image/np.max(image)
-        image = self.inp_transforms(image)
+        image = self.inp_transforms(image[np.newaxis,...])
+        print(type(image))
         mask_path = os.path.join(
             self.root_dir, 'seg/', self.samples[idx] + '_segmented.tiff')
         print('opened '+ mask_path)
-        mask = TiffReader(mask_path).data[1,:,:,:]
-        mask = transforms.ToTensor()(mask.astype(np.float32))
-
+        mask = TiffReader(mask_path).data[0,:,:,:]
+        mask[mask>0] = 1
+        mask = transforms.ToTensor()(mask[np.newaxis,...].astype(np.float32))
+        print(mask.shape)
+        seed = np.random.randint(5000)
         if self.transform is not None:
             # Note: using seeds to ensure the same random transform is applied to
             # the image and mask
-            seed = torch.seed()
-            torch.manual_seed(seed)
+            self.transform.set_random_state(seed)
             image = self.transform(image)
-            torch.manual_seed(seed)
+            self.transform.set_random_state(seed)
             mask = self.transform(mask)
         if self.img_transform is not None:
             image = self.img_transform(image)
@@ -66,13 +68,27 @@ if __name__ == '__main__':
     datadir = '/mnt/efs/dlmbl/G-bs/AvL/'
 
     transform = transforms.Compose([
+            transforms.RandRotate90(prob = 0.33, spatial_axes = (1,2)),
+            transforms.RandSpatialCrop((150,150,150), random_size = False),
             transforms.RandRotate(prob = 0.1),
             transforms.RandAxisFlip(prob = 0.75),
-            transforms.RandRotate90(prob = 0.33, spatial_axes = (1,2)),
-            transforms.RandSpatialCrop(roi_size = [50,50,50])
     ])
 
     testdataset = NucleiDataset(root_dir=datadir, transform = transform, traintestval = 'test')
     image, mask = testdataset[0]
 
     print('you did it', image.dtype, mask.dtype, image.shape, mask.shape)
+
+    # # For viewing random patches
+    # import napari
+    # import random
+
+    # v = napari.Viewer()
+
+    # for i in range(100):
+    #     random_index = random.randint(0, len(testdataset))
+    #     data, mask = testdataset[random_index]
+    #     v.add_image(data, name="data")
+    #     v.add_labels(np.uint8(mask), name="mask", opacity=0.25)
+    #     input("Press Enter to continue...")
+    #     v.layers.clear()
