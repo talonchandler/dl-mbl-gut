160ae0e562488cce50a504bc38266dbe3e4eef47
Aaron van Loon <aaronpvanloon@g.ucla.edu>
2024-08-31 20:01:09 +0000

    edits to dataloader_avl.py

--- src/dl_mbl_gut/dataloader_avl.py
+++ src/dl_mbl_gut/dataloader_avl.py
@@ -1,43 +1,32 @@
-from torch.utils.data import Dataset
+import numpy as np
 import os
 from aicsimageio.readers import TiffReader
-
+import torch
+from torch.utils.data import Dataset
+import pandas as pd
+from monai import transforms
 
 class NucleiDataset(Dataset):
     """A PyTorch dataset to load cell images and nuclei masks"""
 
-    def __init__(self, pop_mean = 1, pop_std = 1, root_dir=".", transform=None, img_transform=None,):
+    def __init__(self, root_dir=".", transform=None, img_transform=None, traintestval = None,):
         self.root_dir = root_dir  # the directory with all the training samples
-        self.pop_mean = pop_mean # population mean of the dataset for normalization
-        self.pop_std = pop_std # population mean of the dataset for normalization
-        self.samples = os.listdir(self.root_dir + 'raw/')  # list the samples
+        self.df = pd.read_csv(self.root_dir + 'celldata.csv') #info about all of the cells
+        self.samples = self.df[self.df.trainclass == traintestval].cell.to_list()  # list the samples
+        
         self.transform = (
             transform  # transformations to apply to both inputs and targets
         )
 
         self.img_transform = img_transform  # transformations to apply to raw image only
         #  transformations to apply just to inputs
-        inp_transforms = transforms.Compose(
+        self.inp_transforms = transforms.Compose(
             [
-                transforms.Normalize(0.5, 0.5),
+                transforms.NormalizeIntensity(0.5, 0.5),
                 transforms.ToTensor(),
             ]
         )
 
-        self.loaded_imgs = [None] * len(self.samples)
-        self.loaded_masks = [None] * len(self.samples)
-        for sample_ind in range(len(self.samples)):
-            img_path = os.path.join(
-                self.root_dir, 'raw/', self.samples[sample_ind]
-            )
-            image = TiffReader(img_path).data
-            normim = image/np.max(image)
-            self.loaded_imgs[sample_ind] = inp_transforms(normim.astype('float32'))
-            mask_path = os.path.join(
-                self.root_dir, 'seg/', self.samples[sample_ind].split('_raw')[0], "_segmented.tiff"
-            )
-            mask = TiffReader(mask_path).data
-            self.loaded_masks[sample_ind] = transforms.ToTensor()(mask)
 
     # get the total number of samples
     def __len__(self):
@@ -47,8 +36,18 @@ class NucleiDataset(Dataset):
     def __getitem__(self, idx):
         # we'll be using Pillow library for reading files
         # since many torchvision transforms operate on PIL images
-        image = self.loaded_imgs[idx]
-        mask = self.loaded_masks[idx]
+        img_path = os.path.join(
+        self.root_dir, 'raw/', self.samples[idx] + '_raw.tiff')
+        image = TiffReader(img_path).data[1,:,:,:]
+        print('opened ' + img_path)
+        image = image/np.max(image)
+        image = self.inp_transforms(image)
+        mask_path = os.path.join(
+            self.root_dir, 'seg/', self.samples[idx] + '_segmented.tiff')
+        print('opened '+ mask_path)
+        mask = TiffReader(mask_path).data[1,:,:,:]
+        mask = transforms.ToTensor()(mask.astype(np.float32))
+
         if self.transform is not None:
             # Note: using seeds to ensure the same random transform is applied to
             # the image and mask
@@ -62,30 +61,18 @@ class NucleiDataset(Dataset):
         return image, mask
 
 
+if __name__ == '__main__':
 
+    datadir = '/mnt/efs/dlmbl/G-bs/AvL/'
 
+    transform = transforms.Compose([
+            transforms.RandRotate(prob = 0.1),
+            transforms.RandAxisFlip(prob = 0.75),
+            transforms.RandRotate90(prob = 0.33, spatial_axes = (1,2)),
+            transforms.RandSpatialCrop(roi_size = [50,50,50])
+    ])
 
-def get_ds_stats(
-        imdir: str,
-        stats: list,
-):
-    statdict = {}
-    count = 0
-    howmany = len(os.listdir(imdir))
-    for i in os.listdir(imdir):
-        im = TiffReader(imdir + i)
-        if (len(stats)>1):
-            imdata = im.data
-        statdict[i] = {}
-        if 'mean' in stats:
-            mean = np.mean(imdata)
-            statdict[i]['mean'] = mean
-        if 'std' in stats:
-            std = np.std(imdata)
-            statdict[i]['std'] = std
-        if 'shape' in stats:
-            shape = im.shape
-            statdict[i]['shape'] = shape
-        count = count +1
-        print(f'Finished_{count}/{howmany}')
-    return statdict
+    testdataset = NucleiDataset(root_dir=datadir, transform = transform, traintestval = 'test')
+    image, mask = testdataset[0]
+
+    print('you did it', image.dtype, mask.dtype, image.shape, mask.shape)
