1cfc09f81a8c05ec23d9034658e2b7bf10fd219f
Talon Chandler <talonchandler@gmail.com>
2024-08-31 15:43:44 +0000

    two dataloaders

--- scripts/2d-train.py
+++ scripts/2d-train.py
@@ -12,22 +12,29 @@ from dl_mbl_gut import dataloader, model, train, evaluation
 
 # Input paths
 base_path = Path("/mnt/efs/dlmbl/G-bs/data/")
-dataset_path = Path("all-downsample-2x.zarr")
+dataset_path = base_path / Path("all-downsample-2x.zarr")
+split_path = base_path / Path("all-downsample-2x-split.csv")
 useful_chunk_path = base_path / Path("all-downsample-2x-masks-only.csv")  # everything
 
 logger = SummaryWriter("runs/Unet")
 
 transform = RandRotate(range_x=np.pi / 8, prob=1.0, padding_mode="zeros")
 
-dataset = dataloader.GutDataset(
-    base_path / dataset_path,
-    useful_chunk_path=useful_chunk_path,
-    transform=transform,
-    patch_size=428,
-    ndim=2,
-)
+train_dataset, val_dataset = [
+    dataloader.GutDataset(
+        dataset_path,
+        split_path,
+        split_mode=split_mode,
+        useful_chunk_path=useful_chunk_path,
+        transform=transform,
+        patch_size=428,
+        ndim=2,
+    )
+    for split_mode in ["train", "val"]
+]
 
-dataloader = DataLoader(dataset, batch_size=10, shuffle=True)
+train_dataloader = DataLoader(train_dataset, batch_size=10, shuffle=True)
+val_dataloader = DataLoader(val_dataset, batch_size=10, shuffle=False)
 
 model = model.UNet(
     depth=4,
